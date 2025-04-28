d9e9874010cfb9f67f7fc96abace4cd6c1cdd52d
Ekrem Guzelyel <ekremguzelyel@hotmail.com>
2025-04-27 18:14:57 -0700

    saving the basics

---
+++ questions.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from dotenv import load_dotenv
+from pprint import pprint
+import requests
+import os
+import logging
+
+logger = logging.getLogger()
+load_dotenv()
+
+# Turn this into a class structure.
+#  And then, also try to add tests.
+def get_question():
+
+    pass
